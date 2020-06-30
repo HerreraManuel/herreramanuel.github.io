7a5d7afca0afe3b3073a6ec8c381f02790c5ab25
HerreraManuel <mherrera.jm@gmail.com>
2020-06-29 21:25:50 -0700

    Updates

---
+++ component---src-pages-work-js-3c127c312a90de3a6b32.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{Ofmp:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return o}));var r=e("q1tI"),u=e.n(r);function o(){return u.a.createElement("div",null,"Work")}}}]);
+//# sourceMappingURL=component---src-pages-work-js-3c127c312a90de3a6b32.js.map
