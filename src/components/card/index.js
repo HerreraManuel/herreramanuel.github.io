8b3d4a3497c0c9a283f6b2127d1e4819fdeeb8cb
HerreraManuel <mherrera.jm@gmail.com>
2020-10-30 02:25:26 -0700

    add image and fixed name spacing

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import "./stylized.scss"
+import './stylized.scss'
+import self from '../../../static/self.png'
 
 const CardInfo = () => {
   return (
     <div className={'content'}>
       <div className={'card'}>
         <div className={'outline'}>
-          <img src=""></img>
+          <img src={self} alt="self"></img>
           <div className={'name'}>
             <h1>Manuel Herrera</h1>
-            <h3>Programmer</h3>
+            <h3>Programmer, Musician, Dedicated Coomer</h3>
             <div className={'info'}>
-              <p>I like to make things</p>
+              <p>please go away please go awayplease go awayplease go awayplease go awayplease go away</p>
             </div>
           </div>
         </div>
