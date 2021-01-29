de2f3b2ebbb6bd57844143396f20bbb05f1459dd
Manuel Herrera <mherrera.jm@gmail.com>
2021-01-28 18:30:14 -0800

    clean portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,5 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`,
-  {
-    resolve: `gatsby-plugin-material-ui`,
-    options: {
-      stylesProvider: {
-        injectFirst: true,
-      },
-    },
-  },
-  'gatsby-plugin-sass'
-]
+    plugins: [
+        `gatsby-theme-material-ui`
+    ]
 }
